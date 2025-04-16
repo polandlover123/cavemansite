5f32ae26b838ab95aa5ce106e6b2c7f18b35d46c
spacefault <74156492+spacefault@users.noreply.github.com>
2025-04-15 23:51:57 -0600

    ahhhhhhh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,21 @@
 <main>
   <div id="text"><div id="text">
     <div class="line">
-      <p class="word">Caveman</p>
+      <p class="word">APRIL 15 2025</p>
     </div>
   
     <div class="line">
-      <p class="word">Website</p>
+      <p class="word">(C) 2025 CAVEMAN SOFTWARE</p>
+      <p class="word">FOR IMMEDIATE RELEASE</p>
     </div>
   
     <div class="line">
-      <p class="word">Under Construction</p>
+      <p class="word">CALGARY, AB — CAVEMAN SOFTWARE, makers of innovative software like CAVEMAN LINUX <br> and PARLIAMENT BOMB, announces it's new website, coming soon. Home to the future of <br> software, CAVEMAN SOFTWARE hopes to redesign the future of computing using new and <br> upcoming technologies. You are now witnessing computer history in real time.</p>
     </div>
+
+    <div class="line">
+    	<p class="word">https://www.CAVEMANSOFTWARE.org<br>@CAVEMANSOFTWARE on instagram.</p>
+    <div>
   </div>
   <div>
 </main>
